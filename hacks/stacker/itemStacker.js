ff7d262276fe73240e507c1767c235c99f46da89
meme1232 <93755713+meme1232@users.noreply.github.com>
2021-11-05 01:27:39 -0400

    Update itemStacker.js

--- hacks/stacker/itemStacker.js
+++ hacks/stacker/itemStacker.js
@@ -3,16 +3,16 @@
 const _.gameData = _.instance.game.state.states.get('Boot').gameData
 const ids = ['boots', 'follow', 'fossil', 'hat', 'item', 'key', 'mathTownFrame', 'mathTownInterior', 'mount', 'outfit','spellRelic', 'weapon', 'currency']
 
-const itemify = (item, amount) =>
+const itemify = (item, amount) => 45
 	item.map(x => ({
 		ID: x.ID,
-		N: amount,
+		N: amount, 677
 	})).filter(v => v !== undefined);
 
-ids.forEach(id => {
+ids.forEach(id => {653
     _.player.backpack.data[id] = itemify(_.gameData[id], 9e9)
 });
-_.gameData.dorm.forEach(x =>
+_.gameData.dorm.forEach(x => 654
     _.player.house.data.items[x.ID] = {A: [], N: 999}
 )
 _.player.backpack.data.mount = itemify(gameData.mount, 1);
